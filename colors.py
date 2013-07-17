9faa22201d49489358a33f46147f6221785dab4e
Aaron Meurer <asmeurer@gmail.com>
2013-07-16 20:08:57 -0500

    Add basic colors example

--- colors.py
+++ colors.py
@@ -4,13 +4,30 @@ def colors(stdscr):
     colors = {getattr(curses, name): name for name in dir(curses) if
         name.startswith("COLOR_")}
 
-    stdscr.addstr(str(curses.can_change_color()))
+    stdscr.addstr(0, 0, "Press c")
+    for backnum in range(1, 8):
+        backcolor = colors[backnum]
+        for forenum in range(1, 8):
+            forecolor = colors[forenum]
+            curses.init_pair(forenum, forenum, backnum)
+            stdscr.addstr(forenum, 0, "%s on %s" % (forecolor, backcolor),
+                curses.color_pair(forenum))
+        stdscr.refresh()
+        get_c(stdscr)
 
-    stdscr.refresh()
+    stdscr.addstr(0, 0, "Press q")
+    get_q(stdscr)
 
+def get_q(stdscr):
     while True:
         c = stdscr.getch()
         if c == ord('q'):
+            break
+
+def get_c(stdscr):
+    while True:
+        c = stdscr.getch()
+        if c == ord('c'):
             break
 
 curses.wrapper(colors)
