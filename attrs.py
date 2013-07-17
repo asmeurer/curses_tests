0c41e152ea9e9365b9131fa0586a5c069428450b
Aaron Meurer <asmeurer@gmail.com>
2013-07-16 19:35:07 -0500

    Add attrs example

---
+++ attrs.py
@@ -0,0 +1,18 @@
+import curses
+
+def highlighting(stdscr):
+    i = 0
+    for name in dir(curses):
+        if name.startswith("A_"):
+            stdscr.addstr(i, 0, "%s: " % name)
+            stdscr.addstr("Example text", getattr(curses, name))
+            i += 1
+
+    stdscr.refresh()
+
+    while True:
+        c = stdscr.getch()
+        if c == ord('q'):
+            break
+
+curses.wrapper(highlighting)
