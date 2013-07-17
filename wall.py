89c1573f8d3bca04b52ccd29c670a8807b36a58e
Aaron Meurer <asmeurer@gmail.com>
2013-07-16 23:00:36 -0500

    Add wall

---
+++ wall.py
@@ -0,0 +1,26 @@
+import curses
+import curses.ascii
+
+def main(stdscr):
+    curses.curs_set(0)
+
+    display_txt(stdscr)
+
+def display_txt(stdscr):
+    height, width = stdscr.getmaxyx()
+    while True:
+        c = get_ch(stdscr)
+        if not c:
+            continue
+        for i in range(height):
+            stdscr.addstr(i, 0, c*(width - 1))
+
+def get_ch(stdscr):
+    try:
+        c = stdscr.getkey()
+    except curses.error:
+        return None
+    if curses.ascii.isprint(c):
+        return c
+
+curses.wrapper(main)
